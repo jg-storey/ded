588f398b495aea5dc37e0d1e32c879faf45ba081
James Storey <69622675+jg-storey@users.noreply.github.com>
2022-05-10 22:59:03 +1200

    v1.0

---
+++ DED.ino
@@ -0,0 +1,327 @@
+/*  DED v1.0 Created by James Storey in May 2022, github.com/jg-storey/ded
+ *  Arduino code to display F-16 Data Entry Display and F-18 Upfront Controls information from DCS-BIOS
+ *  on a ER-OLEDM028-1 2.8" 256x64 OLED display from BuyDisplay.com
+ *  https://www.buydisplay.com/yellow-2-8-inch-arduino-raspberry-pi-oled-display-module-256x64-spi
+ *  Tested with DCS-BIOS v0.10.0
+ *  https://github.com/dcs-bios/dcs-bios/releases
+ *  
+ *  A toggle switch (i.e. not a pushbutton) wired between pin D2 and GND allows the user to select between the F-16 and F-18 modes.
+ *  If a switch is not connected, the mode defaults to the F-16.
+ *  The selected mode activates after disconnecting and connecting to the Arduino in the DCS-BIOS Hub, or by resetting the Arduino.
+ *  There is probably a better way to change modes using an interrupt.
+ *  
+ *  The F-18 display also includes a fuel reading and a master arm indicator. 
+ *  There is still some space to add other items in both modes.
+ *  
+ *  Known issues. The DEST page in the LIST menu comes up blank. This appears to be an issue with DCS-BIOS.
+ *  When entering data between the highlighted asterisks the field does not highlight. I haven't figured out an efficient way to do this yet.
+ */
+
+/*
+  Tell DCS-BIOS to use a serial connection and use interrupt-driven
+  communication. The main program will be interrupted to prioritize
+  processing incoming data.
+  
+  This should work on any Arduino that has an ATMega328 controller
+  (Uno, Pro Mini, many others).
+ */
+#define DCSBIOS_IRQ_SERIAL
+
+
+#include "DcsBios.h"
+
+/***************************************************
+//Web: http://www.buydisplay.com
+EastRising Technology Co.,LTD
+Examples for ER-OLEDM28-1
+Display is Hardward SPI 4-Wire SPI Interface 
+Tested and worked with:
+Works with Arduino 1.6.0 IDE  
+Test OK : Arduino DUE,Arduino mega2560,Arduino UNO Board 
+****************************************************/
+
+/*
+ Note:The module needs to be jumpered to an SPI interface. R19,R23 Short and R18,R20 Open  
+ Unused signal pin Recommended to connect to GND
+  == Hardware connection ==
+    OLED   =>    Arduino
+  *1. GND    ->    GND
+  *2. VCC    ->    3.3
+  *4. SCL    ->    SCK
+  *5. SDI    ->    MOSI
+  *14. DC     ->    9
+  *15. RES    ->    8  
+  *16. CS     ->    10
+*/
+
+
+
+#include "er_oled.h"
+
+
+boolean f16 = true; // flag to display F-16 DED
+boolean f18 = false; // flag to display F-18 UFC
+boolean isList = false; //flag to indicate if we are on the LIST or MISC DED pages
+
+//F-16 DCS BIOS Functions
+
+void onDedLine1Change(char* newValue) {
+    /* your code here */
+    if(f16){
+      //Set isList to true if we are on the LIST or MISC pages
+      if(newValue[11]=='I' && newValue[12]=='S'){
+        isList=true;
+      }
+      else{
+        isList=false;
+      }
+      er_oled_dedstring(0,0,newValue,0); 
+    }
+}
+DcsBios::StringBuffer<29> dedLine1Buffer(0x4500, onDedLine1Change);
+
+void onDedLine2Change(char* newValue) {
+    /* your code here */
+    if(f16){
+      er_oled_dedstring(0,13,newValue,0); 
+      if(isList){
+        //Highlight the menu numbers
+        er_oled_string(0,13,"1",1);
+        er_oled_string(6*8,13,"2",1);
+        er_oled_string(12*8,13,"3",1);
+        er_oled_string(18*8,13,"R",1);
+      }
+    }  
+}
+DcsBios::StringBuffer<29> dedLine2Buffer(0x451e, onDedLine2Change);
+
+
+void onDedLine3Change(char* newValue) {
+    /* your code here */
+    if(f16){
+      er_oled_dedstring(0,26,newValue,0); 
+      if(isList){
+        er_oled_string(0,26,"4",1);
+        er_oled_string(6*8,26,"5",1);
+        er_oled_string(12*8,26,"6",1);
+        er_oled_string(18*8,26,"E",1);
+      }
+    }  
+}
+DcsBios::StringBuffer<29> dedLine3Buffer(0x453c, onDedLine3Change);
+
+void onDedLine4Change(char* newValue) {
+    /* your code here */
+    if(f16){
+      er_oled_dedstring(0,39,newValue,0);  
+      if(isList){
+        er_oled_string(0,39,"7",1);
+        er_oled_string(6*8,39,"8",1);
+        er_oled_string(12*8,39,"9",1);
+        er_oled_string(18*8,39,"0",1);
+      }
+    }
+}
+DcsBios::StringBuffer<29> dedLine4Buffer(0x455a, onDedLine4Change);
+
+void onDedLine5Change(char* newValue) {
+    /* your code here */
+    if(f16){
+      er_oled_dedstring(0,52,newValue,0); 
+    }  
+}
+DcsBios::StringBuffer<29> dedLine5Buffer(0x4578, onDedLine5Change);
+
+
+
+
+// F-18 DCS BIOS Functions
+void onUfcComm1DisplayChange(char* newValue) {
+    /* your code here */
+    if(f18){
+      er_oled_string(0*8,52,newValue,0);
+    }
+}
+DcsBios::StringBuffer<2> ufcComm1DisplayBuffer(0x7424, onUfcComm1DisplayChange);
+
+
+void onUfcComm2DisplayChange(char* newValue) {
+    /* your code here */
+    if(f18){
+      er_oled_string(5*8,52,newValue,0);
+    }
+}
+DcsBios::StringBuffer<2> ufcComm2DisplayBuffer(0x7426, onUfcComm2DisplayChange);
+
+
+void onUfcOptionCueing1Change(char* newValue) {
+    /* your code here */
+    if(f18){
+      er_oled_string(12*8,0,newValue,0);
+    }  
+}
+DcsBios::StringBuffer<1> ufcOptionCueing1Buffer(0x7428, onUfcOptionCueing1Change);
+
+
+void onUfcOptionCueing2Change(char* newValue) {
+    /* your code here */
+    if(f18){
+      er_oled_string(12*8,13,newValue,0);
+    }
+}
+DcsBios::StringBuffer<1> ufcOptionCueing2Buffer(0x742a, onUfcOptionCueing2Change);
+
+
+void onUfcOptionCueing3Change(char* newValue) {
+    /* your code here */
+    if(f18){
+      er_oled_string(12*8,26,newValue,0);
+    }  
+}
+DcsBios::StringBuffer<1> ufcOptionCueing3Buffer(0x742c, onUfcOptionCueing3Change);
+
+
+void onUfcOptionCueing4Change(char* newValue) {
+    /* your code here */
+    if(f18){
+      er_oled_string(12*8,39,newValue,0);
+    }
+}
+DcsBios::StringBuffer<1> ufcOptionCueing4Buffer(0x742e, onUfcOptionCueing4Change);
+
+void onUfcOptionCueing5Change(char* newValue) {
+    /* your code here */
+    if(f18){
+      er_oled_string(12*8,52,newValue,0);
+    }
+}
+DcsBios::StringBuffer<1> ufcOptionCueing5Buffer(0x7430, onUfcOptionCueing5Change);
+
+
+void onUfcOptionDisplay1Change(char* newValue) {
+    /* your code here */
+    if(f18){
+      er_oled_string(13*8,0,newValue,0);
+    }
+}
+DcsBios::StringBuffer<4> ufcOptionDisplay1Buffer(0x7432, onUfcOptionDisplay1Change);
+
+
+void onUfcOptionDisplay2Change(char* newValue) {
+    /* your code here */
+    if(f18){
+      er_oled_string(13*8,13,newValue,0);
+    }
+}
+DcsBios::StringBuffer<4> ufcOptionDisplay2Buffer(0x7436, onUfcOptionDisplay2Change);
+
+void onUfcOptionDisplay3Change(char* newValue) {
+    /* your code here */
+    if(f18){
+      er_oled_string(13*8,26,newValue,0);
+    }
+}
+DcsBios::StringBuffer<4> ufcOptionDisplay3Buffer(0x743a, onUfcOptionDisplay3Change);
+
+void onUfcOptionDisplay4Change(char* newValue) {
+    /* your code here */
+    if(f18){
+      er_oled_string(13*8,39,newValue,0);
+    }
+}
+DcsBios::StringBuffer<4> ufcOptionDisplay4Buffer(0x743e, onUfcOptionDisplay4Change);
+
+void onUfcOptionDisplay5Change(char* newValue) {
+    /* your code here */
+    if(f18){
+      er_oled_string(13*8,52,newValue,0);
+    }
+}
+DcsBios::StringBuffer<4> ufcOptionDisplay5Buffer(0x7442, onUfcOptionDisplay5Change);
+
+
+void onUfcScratchpadNumberDisplayChange(char* newValue) {
+    /* your code here */
+    if(f18){
+      er_oled_string(3*8,0,newValue,0);
+    }
+}
+DcsBios::StringBuffer<8> ufcScratchpadNumberDisplayBuffer(0x7446, onUfcScratchpadNumberDisplayChange);
+
+
+
+void onUfcScratchpadString1DisplayChange(char* newValue) {
+    /* your code here */
+    if(f18){
+      er_oled_string(0*8,0,newValue,0);
+    }
+}
+DcsBios::StringBuffer<2> ufcScratchpadString1DisplayBuffer(0x744e, onUfcScratchpadString1DisplayChange);
+
+void onUfcScratchpadString2DisplayChange(char* newValue) {
+    /* your code here */
+    if(f18){
+      er_oled_string(2*8,0,newValue,0);
+    }
+}
+DcsBios::StringBuffer<2> ufcScratchpadString2DisplayBuffer(0x7450, onUfcScratchpadString2DisplayChange);
+
+
+void onIfeiFuelUpChange(char* newValue) {
+    /* your code here */
+    if(f18){
+      er_oled_string(20*8,26,newValue,0);
+    }
+}
+DcsBios::StringBuffer<6> ifeiFuelUpBuffer(0x748a, onIfeiFuelUpChange);
+
+void onMasterModeAaLtChange(unsigned int newValue) {
+    /* your code here */
+}
+DcsBios::IntegerBuffer masterModeAaLtBuffer(0x740c, 0x0200, 9, onMasterModeAaLtChange);
+
+void onMasterArmSwChange(unsigned int newValue) {
+    /* your code here */
+    if(f18){
+      if(newValue){
+      er_oled_string(20*8,0," ARM ",1);
+    }else{
+      er_oled_string(20*8,0," SAFE ",0);
+    }
+  }
+}
+DcsBios::IntegerBuffer masterArmSwBuffer(0x740c, 0x2000, 13, onMasterArmSwChange);
+
+
+
+void setup() {
+
+  //set D2 as a switch to select F-16 or F-18 displays. If unconnected it defaults to F-16.
+  pinMode(2, INPUT_PULLUP);
+  f16 = digitalRead(2);
+  f18 = !f16;
+
+  /* initialize the display */
+  er_oled_begin();
+  er_oled_clear();
+  er_oled_string(12*8,13,"DED V1.0",1);
+  er_oled_string(4*8,26,"GITHUB.COM/JG-STOREY/DED",0);
+  if(f16){
+    er_oled_string(11*8,52,"F-16 MODE",0);
+  }
+  else if(f18){
+    er_oled_string(11*8,52,"F/A-18 MODE",0);
+  }
+  delay(2000);
+  er_oled_clear();
+  
+  DcsBios::setup();
+  
+}
+
+
+void loop() {
+
+DcsBios::loop();
+ 
+}
---
+++ er_oled.cpp
@@ -0,0 +1,215 @@
+/***************************************************
+//Web: http://www.buydisplay.com
+EastRising Technology Co.,LTD
+****************************************************/
+
+//May 2022. Contains modifications by James Storey to imitate F-16 Data Entry Display.
+
+#include <SPI.h>
+#include "er_oled.h"
+
+
+void command(uint8_t cmd)
+{
+  digitalWrite(OLED_DC, LOW);
+  digitalWrite(OLED_CS, LOW);
+  SPI.transfer(cmd);
+  digitalWrite(OLED_CS, HIGH);
+}
+
+void data(uint8_t dat)
+{
+  digitalWrite(OLED_DC, HIGH);
+  digitalWrite(OLED_CS, LOW );
+  SPI.transfer(dat);
+  digitalWrite(OLED_CS, HIGH);
+}
+
+
+void er_oled_begin()
+{
+  pinMode(OLED_RST, OUTPUT);
+  pinMode(OLED_DC, OUTPUT);
+  pinMode(OLED_CS, OUTPUT);
+  SPI.begin();
+    
+  SPI.setClockDivider(SPI_CLOCK_DIV128);
+    
+  digitalWrite(OLED_CS, LOW);
+  digitalWrite(OLED_RST, HIGH);
+  delay(10);
+  digitalWrite(OLED_RST, LOW);
+  delay(10);
+  digitalWrite(OLED_RST, HIGH);
+    
+  
+  command(0xFD); /*SET COMMAND LOCK*/ 
+  data(0x12); /* UNLOCK */ 
+  command(0xAE); /*DISPLAY OFF*/ 
+  command(0xB3);/*DISPLAYDIVIDE CLOCKRADIO/OSCILLATAR FREQUANCY*/ 
+  data(0x91); command(0xCA); /*multiplex ratio*/ 
+  data(0x3F); /*duty = 1/64*/ 
+  command(0xA2); /*set offset*/ 
+  data(0x00);
+  command(0xA1); /*start line*/ 
+  data(0x00); 
+  command(0xA0); /*set remap*/
+  data(0x14); 
+  data(0x11);
+  	
+  command(0xAB); /*funtion selection*/ 
+  data(0x01); /* selection external vdd */ 
+  command(0xB4); /* */ 
+  data(0xA0);
+  data(0xfd); 
+  command(0xC1); /*set contrast current */ 
+  data(0x80); 
+  command(0xC7); /*master contrast current control*/ 
+  data(0x0f); 
+  	
+  command(0xB1); /*SET PHASE LENGTH*/
+  data(0xE2); 
+  command(0xD1); /**/
+  data(0x82); 
+  data(0x20); 
+  command(0xBB); /*SET PRE-CHANGE VOLTAGE*/ 
+  data(0x1F);
+  command(0xB6); /*SET SECOND PRE-CHARGE PERIOD*/
+  data(0x08); 
+  command(0xBE); /* SET VCOMH */ 
+  data(0x07); 
+  command(0xA6); /*normal display*/ 
+  command(0xAF); /*display ON*/  
+}
+
+void er_oled_SetWindow(uint8_t Xstart, uint8_t Ystart, uint8_t Xend, uint8_t Yend)
+{ 
+  command(0x15);
+  data(Xstart+0x1c);
+  data(Xend+0x1c);
+  command(0x75);
+  data(Ystart);
+  data(Yend);
+  command(0x5c);//write ram command
+}
+
+void er_oled_clear()
+{int i,row;
+  command(0x15);
+  data(0x00); //col start
+  data(0x77); //col end 
+  command(0x75);
+  data(0x00); //row start
+  data(0x7f);  //row end 
+  command(0x5c); 
+  for (row = 0; row < 128; row++) {              
+        for(i = 0; i< 240; i++ ) {
+          data(0x00);// write data       
+        }        
+  }
+}
+
+// Modified by James Storey for characters with 11 rows of pixels (instead of 16 rows).
+void er_oled_char(uint8_t x, uint8_t y, const char  *acsii, uint8_t mode)
+{ uint8_t i,str;uint16_t OffSet;
+  x=x/4;
+  
+  OffSet = (*acsii - 32)*11;
+  
+  er_oled_SetWindow(x, y, x+1, y+11);
+  for (i=0;i<11;i++)
+  {     str =pgm_read_byte(&AsciiLib[OffSet + i]);  
+        if(mode) str=~str;
+         Data_processing (str);             
+  }
+}
+
+void er_oled_string(uint8_t x, uint8_t y, const char *pString,  uint8_t Mode)
+{     
+  while(1)
+  {
+        if (*pString == 0)
+        {
+            return;
+        }
+            er_oled_char(x, y, pString,Mode);
+            x += 8;
+            pString += 1;              
+  }   
+}
+
+//Created by James Storey. Similar to er_oled_string but changes asterisk color and only prints first 24 characters
+//because sometimes the DCS bios string contains extra characters.
+void er_oled_dedstring(uint8_t x, uint8_t y, const char *pString,  uint8_t Mode)
+{     
+  while(1)
+  {
+        if (*pString == 0)
+        {
+            return;
+        }
+            if(*pString==42){
+              //character is an asterisk so make it dark on light background.
+              er_oled_char(x,y,pString,1);
+            }else{
+              er_oled_char(x,y,pString,Mode);
+            }
+            x += 8;
+            if(x>24*8){
+              return;
+            }
+            pString += 1;              
+  }   
+}
+
+void Data_processing(uint8_t temp)  //turns 1byte B/W data to 4 bye gray data  with 8 Pixel
+{uint8_t temp1,temp2;
+
+  if(temp&0x80)temp1=0xf0;
+  else temp1=0x00;
+  if(temp&0x40)temp2=0x0f;
+  else temp2=0x00;
+  temp1=temp1|temp2;
+  data(temp1); //Pixel1,Pixel2
+  if(temp&0x20)temp1=0xf0;
+  else temp1=0x00;
+  if(temp&0x10)temp2=0x0f;
+  else temp2=0x00;
+  temp1=temp1|temp2;
+  data(temp1);  //Pixel3,Pixel4                
+  if(temp&0x08)temp1=0xf0;
+  else temp1=0x00;
+  if(temp&0x04)temp2=0x0f;
+  else temp2=0x00;
+  temp1=temp1|temp2;
+  data(temp1);  //Pixel5,Pixel6               
+  if(temp&0x02)temp1=0xf0;
+  else temp1=0x00;
+  if(temp&0x01)temp2=0x0f;
+  else temp2=0x00;
+  temp1=temp1|temp2;
+  data(temp1);  //Pixel7,Pixel8               
+}
+
+void er_oled_bitmap_mono(const uint8_t * pBuf)
+{ uint8_t row,col,dat; 
+  er_oled_SetWindow(0, 0, 255/4, 63); 
+  for (row = 0; row < 64; row++) {              
+        for(col = 0;col<256/8; col++ ) {
+        dat=(pgm_read_byte(pBuf));
+        *pBuf++;  
+        Data_processing(dat);               
+        }
+  }    	
+}
+
+void er_oled_bitmap_gray(const uint8_t * pBuf)
+{   uint8_t row,col; 
+  er_oled_SetWindow(0, 0, 255/4, 63);
+  for (row = 0; row < 64; row++) {              
+        for(col = 0;col<128; col++ ) {
+        data(pgm_read_byte(pBuf));
+        * pBuf++;
+        }
+  }    	
+}
